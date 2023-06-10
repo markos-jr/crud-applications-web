c05f48236900f6c7a5ada1cd468c4382fb69a5b3
markos-jr <markos-jr@outlook.com>
2023-06-10 13:48:43 -0300

    Criando Material Table para Listar Anamolias

--- crud-ui/src/app/anomalias/anomalia-table/anomalia-table.component.ts
+++ crud-ui/src/app/anomalias/anomalia-table/anomalia-table.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+export interface Anomalias {
+  _id: string;
+  equipamento: string;
+  falha: string;
+
+}
+
+
 
 @Component({
   selector: 'app-anomalia-table',
   templateUrl: './anomalia-table.component.html',
   styleUrls: ['./anomalia-table.component.scss']
 })
-export class AnomaliaTableComponent {
+export class AnomaliaTableComponent implements OnInit {
+
+  displayedColumns: string[] = ['_id', 'equipamento', 'falha'];
+
+
+  anomalias: Anomalias[] = [
+    {_id: '1', equipamento: 'CCM1', falha: 'Elétrica'}
+  ];
+
+  constructor(){
+    //this.anomalias = []
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
+
+
--- crud-ui/src/app/anomalias/anomalias.module.ts
+++ crud-ui/src/app/anomalias/anomalias.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { AnomaliasRoutingModule } from './anomalias-routing.module';
 import { AnomaliaTableComponent } from './anomalia-table/anomalia-table.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [
     AnomaliaTableComponent
+
   ],
   imports: [
     CommonModule,
-    AnomaliasRoutingModule
+    AnomaliasRoutingModule,
+    MatTableModule,
+    MatCardModule
   ]
 })
 export class AnomaliasModule { }
